23f0e114faf5554c09e7a333006244fe70ea9a7a
root <mgiraudo@ies21.edu.ar>
2022-10-26 12:30:13 -0300

    Agregar archivos de proyecto.

---
+++ pryComettoSP2_2/frmMain.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace pryComettoSP2_2
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nudDias = new System.Windows.Forms.NumericUpDown();
+            this.txtDistancia = new System.Windows.Forms.TextBox();
+            this.lblDias = new System.Windows.Forms.Label();
+            this.lblDistancia = new System.Windows.Forms.Label();
+            this.lblKm = new System.Windows.Forms.Label();
+            this.mrcCosto = new System.Windows.Forms.GroupBox();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblPrecioKm = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.txtPrecioKm = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDias)).BeginInit();
+            this.mrcCosto.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // nudDias
+            // 
+            this.nudDias.Location = new System.Drawing.Point(173, 50);
+            this.nudDias.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.nudDias.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudDias.Name = "nudDias";
+            this.nudDias.Size = new System.Drawing.Size(52, 20);
+            this.nudDias.TabIndex = 0;
+            this.nudDias.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudDias.ValueChanged += new System.EventHandler(this.nudDias_ValueChanged);
+            // 
+            // txtDistancia
+            // 
+            this.txtDistancia.Location = new System.Drawing.Point(173, 85);
+            this.txtDistancia.Name = "txtDistancia";
+            this.txtDistancia.Size = new System.Drawing.Size(70, 20);
+            this.txtDistancia.TabIndex = 1;
+            this.txtDistancia.TextChanged += new System.EventHandler(this.txtDistancia_TextChanged);
+            // 
+            // lblDias
+            // 
+            this.lblDias.AutoSize = true;
+            this.lblDias.Location = new System.Drawing.Point(137, 52);
+            this.lblDias.Name = "lblDias";
+            this.lblDias.Size = new System.Drawing.Size(30, 13);
+            this.lblDias.TabIndex = 2;
+            this.lblDias.Text = "Días";
+            // 
+            // lblDistancia
+            // 
+            this.lblDistancia.AutoSize = true;
+            this.lblDistancia.Location = new System.Drawing.Point(116, 88);
+            this.lblDistancia.Name = "lblDistancia";
+            this.lblDistancia.Size = new System.Drawing.Size(51, 13);
+            this.lblDistancia.TabIndex = 3;
+            this.lblDistancia.Text = "Distancia";
+            // 
+            // lblKm
+            // 
+            this.lblKm.AutoSize = true;
+            this.lblKm.Location = new System.Drawing.Point(243, 88);
+            this.lblKm.Name = "lblKm";
+            this.lblKm.Size = new System.Drawing.Size(21, 13);
+            this.lblKm.TabIndex = 4;
+            this.lblKm.Text = "km";
+            // 
+            // mrcCosto
+            // 
+            this.mrcCosto.Controls.Add(this.lblTotal);
+            this.mrcCosto.Controls.Add(this.lblPrecioKm);
+            this.mrcCosto.Controls.Add(this.txtTotal);
+            this.mrcCosto.Controls.Add(this.txtPrecioKm);
+            this.mrcCosto.Location = new System.Drawing.Point(64, 158);
+            this.mrcCosto.Name = "mrcCosto";
+            this.mrcCosto.Size = new System.Drawing.Size(264, 106);
+            this.mrcCosto.TabIndex = 5;
+            this.mrcCosto.TabStop = false;
+            this.mrcCosto.Text = "Costo";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(72, 54);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(31, 13);
+            this.lblTotal.TabIndex = 9;
+            this.lblTotal.Text = "Total";
+            // 
+            // lblPrecioKm
+            // 
+            this.lblPrecioKm.AutoSize = true;
+            this.lblPrecioKm.Location = new System.Drawing.Point(41, 28);
+            this.lblPrecioKm.Name = "lblPrecioKm";
+            this.lblPrecioKm.Size = new System.Drawing.Size(62, 13);
+            this.lblPrecioKm.TabIndex = 7;
+            this.lblPrecioKm.Text = "Precio x km";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(109, 51);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(70, 20);
+            this.txtTotal.TabIndex = 8;
+            // 
+            // txtPrecioKm
+            // 
+            this.txtPrecioKm.Location = new System.Drawing.Point(109, 25);
+            this.txtPrecioKm.Name = "txtPrecioKm";
+            this.txtPrecioKm.Size = new System.Drawing.Size(70, 20);
+            this.txtPrecioKm.TabIndex = 6;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 365);
+            this.Controls.Add(this.mrcCosto);
+            this.Controls.Add(this.lblKm);
+            this.Controls.Add(this.lblDistancia);
+            this.Controls.Add(this.lblDias);
+            this.Controls.Add(this.txtDistancia);
+            this.Controls.Add(this.nudDias);
+            this.Name = "frmMain";
+            this.Text = "Ferrocarriles Atlántico-Pacífico";
+            ((System.ComponentModel.ISupportInitialize)(this.nudDias)).EndInit();
+            this.mrcCosto.ResumeLayout(false);
+            this.mrcCosto.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nudDias;
+        private System.Windows.Forms.TextBox txtDistancia;
+        private System.Windows.Forms.Label lblDias;
+        private System.Windows.Forms.Label lblDistancia;
+        private System.Windows.Forms.Label lblKm;
+        private System.Windows.Forms.GroupBox mrcCosto;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblPrecioKm;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.TextBox txtPrecioKm;
+    }
+}
+
---
+++ pryComettoSP2_2/frmMain.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryComettoSP2_2
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void txtDistancia_TextChanged(object sender, EventArgs e)
+        {
+            if (txtDistancia.Text != "")
+            {
+                float precioTotal = Convert.ToInt32(txtDistancia.Text) * 5;
+                if (int.Parse(txtDistancia.Text) >= 100 && nudDias.Value >= 7)
+                {
+                    precioTotal = precioTotal * 0.5f;
+                    txtTotal.Text = precioTotal.ToString();
+                    txtPrecioKm.Text = "2,5";
+                }
+                else
+                {
+                    txtTotal.Text = precioTotal.ToString();
+                    txtPrecioKm.Text = "5";
+                }         
+            }
+            else
+            {
+                txtPrecioKm.Text = "";
+                txtTotal.Text = "";
+            }
+        }
+
+        private void nudDias_ValueChanged(object sender, EventArgs e)
+        {
+            if (txtDistancia.Text != "")
+            {
+                double precioTotal = Convert.ToInt32(txtDistancia.Text) * 5;
+                if (int.Parse(txtDistancia.Text) >= 100 && nudDias.Value >= 7)
+                {
+                    precioTotal = precioTotal * 0.5;
+                    txtTotal.Text = precioTotal.ToString();
+                    txtPrecioKm.Text = "2,5";
+                }
+                else
+                {
+                    txtTotal.Text = precioTotal.ToString();
+                    txtPrecioKm.Text = "5";
+                }
+            }
+            else
+            {
+                txtPrecioKm.Text = "";
+                txtTotal.Text = "";
+            }
+        }
+    }
+}
